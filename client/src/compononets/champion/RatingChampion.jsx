6fe782633071c15a3d5ca6cba830b7629ab70da7
milendrandev <66078288+milendrandev@users.noreply.github.com>
2025-03-24 22:41:45 +0200

    update rating local state correctly

--- client/src/compononets/champion/RatingChampion.jsx
+++ client/src/compononets/champion/RatingChampion.jsx
@@ -7,30 +7,36 @@ import { UserContext } from '../../contexts/UserContext';
 import useFetchFilterByTwo from '../../hooks/useSearchByTwoCriteries';
 
 export default function RatingChampion({ championId }) {
-  const [voted, setVoted] = React.useState({});
+  const [voted, setVoted] = React.useState(false);
   const [value, setValue] = React.useState(2);
 
   const { accessToken, _id } = React.useContext(UserContext);
 
   const { data } = useFetchFilterByTwo("_ownerId", _id, "championId", championId)
 
-  console.log(data);
+  if (data != undefined) {
+    setValue(data[0].ratingValue)
+    console.log(value + " first");
+  }
+
+  console.log(value + " second");
+  //console.log(data);
   //setVoted(data[0].ratingValue)
 
   const handleVote = (event) => {
-
+    setVoted(true);
     ratingService.create(championId, event.target.value, accessToken);
   }
 
   return (
     <Box sx={{ '& > legend': { mt: 0.5 }, color: 'white' }}>
-      {data.length === 0 && data[0] != 404 ?
+      {(data.length === 0 || data[0] == 404) && !voted ?
         <>
           <Rating
             name="simple-controlled"
             value={value}
             onChange={(event) => {
-
+              setValue(event.target.value)
               handleVote(event);
             }}
           />
