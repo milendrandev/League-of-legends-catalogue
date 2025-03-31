2496c9477be8d5310663abcf745f85660096282a
milendrandev <66078288+milendrandev@users.noreply.github.com>
2025-03-31 17:14:36 +0300

    ProfilePage styling

--- client/src/compononets/Authentication/Profile.jsx
+++ client/src/compononets/Authentication/Profile.jsx
@@ -1,5 +1,25 @@
-export default function Profile(params) {
+export default function Profile() {
     return (
-        <></>
+        <>
+            <div className="profile">
+                <div className="profile-container">
+                    <section className="profileSection">
+                        <main className="profileMain">
+                            <h1>Your Name Here</h1>
+                            <p>
+                                Intro text: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                                labore et dolore magna aliqua.
+                            </p>
+                            <p className="start">
+                                Let’s get started
+                            </p>
+                        </main>
+                        <aside className="profileAside">
+                            <p><img src="https://img.freepik.com/premium-vector/avatar-profile-picture-icon-blue-background-flat-design-style-resources-graphic-element-design_991720-653.jpg" alt="Profil Picture" /></p>
+                        </aside>
+                    </section>
+                </div>
+            </div >
+        </>
     )
 }
