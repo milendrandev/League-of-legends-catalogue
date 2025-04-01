8463b3f90b5c584ade5acfde3f7b40e0f242c3f1
milendrandev <66078288+milendrandev@users.noreply.github.com>
2025-04-01 16:57:44 +0300

    Preview Modal on ChampionEdit Images

--- client/src/compononets/champion/EditChampion.jsx
+++ client/src/compononets/champion/EditChampion.jsx
@@ -2,12 +2,17 @@ import { useContext, useEffect, useState } from "react";
 import { Link, useNavigate, useParams } from "react-router";
 import championService from "../../services/championService";
 import { UserContext } from "../../contexts/UserContext";
+import PreviewModal from "./PreviewModal";
 
 export default function EditChampion() {
     const navigate = useNavigate();
     const [champion, setChampion] = useState({});
     const { championId } = useParams();
     const { accessToken } = useContext(UserContext)
+    const [characterImage, setCharacterImage] = useState({});
+    const [coverImage, setCoverImage] = useState({});
+    const [characterChanged, setCharacterChanged] = useState(false);
+    const [coverChanged, setCoverChanged] = useState(false);
 
     useEffect(() => {
         championService.getChampionById(championId)
@@ -20,6 +25,16 @@ export default function EditChampion() {
         console.log(data)
         navigate(`/catalogue`);
     }
+
+    const onCoverChange = (event) => {
+        setCoverChanged(true);
+        setCoverImage(event.target.value)
+    };
+
+    const onCharacterChange = (event) => {
+        setCharacterChanged(true);
+        setCharacterImage(event.target.value)
+    };
 
     return (
         <>
@@ -67,10 +82,30 @@ export default function EditChampion() {
                                 <input type="text" name="title" required defaultValue={champion.title} /> <i>Champion Name</i>
                             </div>
                             <div className="inputBox">
-                                <input type="text" name="coverImageUrl" required defaultValue={champion.coverImageUrl} /> <i>Cover Image Url</i>
+                                <input type="text" name="coverImageUrl" required defaultValue={champion.coverImageUrl} onChange={onCoverChange} />
+                                <i>Cover Image Url</i>
+                                {coverChanged
+                                    ?
+                                    <div className="preview">
+                                        <PreviewModal coverImage={coverImage} />
+                                    </div>
+                                    :
+                                    <div className="preview">
+                                        <PreviewModal coverImage={champion.coverImageUrl} />
+                                    </div>}
                             </div>
                             <div className="inputBox">
-                                <input type="text" name="characterImageUrl" required defaultValue={champion.characterImageUrl} /> <i>Character Image Url</i>
+                                <input type="text" name="characterImageUrl" required defaultValue={champion.characterImageUrl} onChange={onCharacterChange} />
+                                <i>Character Image Url</i>
+                                {characterChanged
+                                    ?
+                                    <div className="preview">
+                                        <PreviewModal characterImage={characterImage} />
+                                    </div>
+                                    :
+                                    <div className="preview">
+                                        <PreviewModal characterImage={champion.characterImageUrl} />
+                                    </div>}
                             </div>
                             <div className="inputBox">
                                 <textarea name="story" required defaultValue={champion.story} /> <i>Lore Story</i>
---
+++ client/src/compononets/champion/PreviewModal.jsx
@@ -0,0 +1,47 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+
+export default function PreviewModal({ coverImage, characterImage }) {
+    const [open, setOpen] = React.useState(false);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    return (
+        <React.Fragment>
+            <Button variant="outlined" sx={{ width: '20%', fontSize: '14px', margin: 0, padding: 0, color: '#0f0', border: 'none' }} onClick={handleClickOpen}>
+                Preview
+            </Button>
+            <Dialog
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+                sx={{color:'#222'}}
+            >
+                <DialogTitle id="alert-dialog-title">
+                    {"Use Google's location service?"}
+                </DialogTitle>
+                <DialogContent>
+                    <DialogContentText id="alert-dialog-description">
+                        <img src={coverImage} alt="" />
+                        <img src={characterImage} alt="" />
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose}>Cancel</Button>
+                </DialogActions>
+            </Dialog>
+        </React.Fragment>
+    );
+}
